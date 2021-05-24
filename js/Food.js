0285dc4f2692fc6355467dccea23ead03b684db5
srujan338 <79457460+srujan338@users.noreply.github.com>
2021-05-24 10:34:05 +0530

    Add files via upload

--- js/Food.js
+++ js/Food.js
@@ -27,7 +27,7 @@ class Food {
       var x=80,y=100;
 
       imageMode(CENTER);
-      
+      image(this.image,x,y,50,50);
 
       if(this.foodStock!=0){
         for(var i=0;i<this.foodStock;i++){
